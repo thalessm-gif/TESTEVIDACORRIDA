50c8a1c4314ea14e343c7a10c2f0a8751e544c98
Thales <thalessm@gmail.com>
2026-04-09 15:07:06 -0300

    Add files via upload

--- avatar-map-data.js
+++ avatar-map-data.js
@@ -10,6 +10,7 @@ window.VIDA_CORRIDA_AVATAR_DATA = {
                    "Ana Milano":  "ana_milano.jpg",
                    "Andre Alvarenga":  "andre_alvarenga.jpg",
                    "Angela Potter":  "angela_potter.jpg",
+                   "Bruna Telmo Alvarenga":  "bruna_alvarenga.jpg",
                    "Camila Magroski":  "camila_magroski.jpg",
                    "Carla Fenzke":  "carla-fenzke.jpg",
                    "Catia Fenzke":  "catia-fenzke.jpg",
@@ -18,9 +19,15 @@ window.VIDA_CORRIDA_AVATAR_DATA = {
                    "Cintia Correa Campos":  "cintia_campos.jpg",
                    "Claudio Corrêa Simões":  "claudio_simoes.jpg",
                    "Eduarda Constantino":  "eduarda_constantino.jpg",
+                   "Fátima Ortiz Crexi":  "fatima_ortiz.jpg",
                    "Felype Nascimento":  "felype-nascimento.jpg",
+                   "Frederico Gomes":  "frederico_gomes.jpg",
+                   "Gabriela Machado Carvalho Zanetti":  "gabriela_zanetti.jpg",
+                   "Graziana Nunes":  "graziana_nunes.jpg",
                    "Janaina Garcia Mascarenhas":  "janaina_mascarenhas.jpg",
+                   "Janaina Jaques De Souza":  "janaina_jaques.jpg",
                    "Joana Ribeiro":  "joana-ribeiro.jpg",
+                   "Josepher Veleda Caetano":  "josepher_veleda.jpg",
                    "Julio Araujo":  "julio-araujo.jpg",
                    "Karen Christie Louro Mano":  "karen_christie.jpg",
                    "Lorraine Rosa":  "lorraine_rosa.jpg",
@@ -28,8 +35,12 @@ window.VIDA_CORRIDA_AVATAR_DATA = {
                    "Luisa Pinto da Silva":  "luisa-pinto-da-silva.jpg",
                    "Miguel Angelo Castro Jr.":  "miguel_castro.jpg",
                    "Nathalia Vergara":  "nathalia-vergara.jpg",
+                   "Pietra Braga Silva":  "pietra_braga.jpg",
+                   "Priscila Duarte Gomes":  "priscila_gomes.jpg",
+                   "Sergio Carrasco":  "sergio_carrasco.jpg",
                    "Simone Castro De Oliveira":  "simone_castro.jpg",
                    "Thales Mirapalheta":  "thales-mirapalheta.jpg",
-                   "Thales Soares Mirapalheta":  "thales-mirapalheta.jpg"
+                   "Thales Soares Mirapalheta":  "thales-mirapalheta.jpg",
+                   "Thiago Edom Nunes":  "thiago_edom.jpg"
                }
 };
